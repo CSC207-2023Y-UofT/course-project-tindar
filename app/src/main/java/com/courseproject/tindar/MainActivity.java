8bc5a6a0555ce722fe789b7c9711645505da445a
chocolatecakes <serena.he.03@gmail.com>
2023-07-23 19:21:40 -0400

    implemented blank nav with placeholders

--- app/src/main/java/com/courseproject/tindar/MainActivity.java
+++ app/src/main/java/com/courseproject/tindar/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.courseproject.tindar.ui.editfilters.EditFiltersActivity;
 import com.courseproject.tindar.ui.editprofile.EditProfileActivity;
+import com.courseproject.tindar.ui.home.HomeFragment;
+import com.courseproject.tindar.SignupFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         Button editProfileButton = findViewById(R.id.button_edit_profile);
         Button editFiltersButton = findViewById(R.id.button_edit_filters);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, blankNav.class);
+//            change blankNav or login name, currently like this since i'm not sure what we want the landing page to be
+//            used to test the blank nav and for peeps to check it out and extend off it
+            intent.putExtra("user_id", "1");
+            startActivity(intent);
+        });
 
         editProfileButton.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
