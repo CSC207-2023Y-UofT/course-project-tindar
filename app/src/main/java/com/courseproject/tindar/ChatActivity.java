f578eebcf7f048ff7f002a36ed943686811d7445
navarhontes <sophiawan01@gmail.com>
2023-07-30 15:59:15 -0400

    Made ChatActivity.java and outlined what needs to be done

---
+++ app/src/main/java/com/courseproject/tindar/ChatActivity.java
@@ -0,0 +1,17 @@
+package com.courseproject.tindar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+
+
+}
