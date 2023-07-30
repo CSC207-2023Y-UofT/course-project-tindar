f341a7184e00d6eb106e834fb7e0ace7ad49bdea
navarhontes <sophiawan01@gmail.com>
2023-07-30 18:17:28 -0400

    Trying and failing to make a nice chat display
    Using a tutorial where they have many nested RelativeLayout blocks, but they're not working for me for some reason?

--- app/src/main/java/com/courseproject/tindar/ChatActivity.java
+++ app/src/main/java/com/courseproject/tindar/ChatActivity.java
@@ -1,15 +1,29 @@
 package com.courseproject.tindar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class ChatActivity extends AppCompatActivity {
+    // Variable here for getting other userID and display name
+    // variable here for storing your userID
+    EditText chatInput;
+    ImageButton sendMessageButton;
+    ImageButton backButton;
+    RecyclerView chatRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        this.chatInput = findViewById(R.id.new_chat_input);
+        this.sendMessageButton = findViewById(R.id.send_message_button);
+        this.backButton = findViewById(R.id.back_button);
+        this.chatRecyclerView = findViewById(R.id.chat_recycler_view);
     }
 
 
