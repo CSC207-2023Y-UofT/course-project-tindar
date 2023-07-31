56d73db5a964f54d90ac3cce7b813930f0d3c1b6
A Bunch Of Atoms <caullinseale@gmail.com>
2023-07-31 18:35:09 -0400

    Added basic security measure on changing account credentials.

--- app/src/main/java/com/courseproject/tindar/controllers/editaccount/EditAccountController.java
+++ app/src/main/java/com/courseproject/tindar/controllers/editaccount/EditAccountController.java
@@ -3,6 +3,8 @@ package com.courseproject.tindar.controllers.editaccount;
 import com.courseproject.tindar.usecases.editaccount.EditAccountInputBoundary;
 import com.courseproject.tindar.usecases.editaccount.EditAccountDsResponseModel;
 
+import java.util.Objects;
+
 public class EditAccountController {
     final EditAccountInputBoundary userInput;
 
@@ -12,6 +14,11 @@ public class EditAccountController {
 
     public EditAccountDsResponseModel getAccount(String userId) {
         return userInput.getAccount(userId);
+    }
+
+    public boolean validateChanges(String userId, String password) {
+        String vPassword = this.userInput.getAccount(userId).getPassword();
+        return Objects.equals(vPassword, password);
     }
 
     public void updateIsActiveStatus(String userId, boolean isActiveStatus) {
