fd97f81a4ab44b8c1dbedbd7d82aee4fcd77ab6b
kresimirgotovac <133273109+kresimirgotovac@users.noreply.github.com>
2023-07-31 22:50:03 -0400

    minor tweaks as per pr comments

--- app/src/main/java/com/courseproject/tindar/controllers/likelist/LikeListController.java
+++ app/src/main/java/com/courseproject/tindar/controllers/likelist/LikeListController.java
@@ -3,16 +3,16 @@ package com.courseproject.tindar.controllers.likelist;
 import com.courseproject.tindar.usecases.likelist.LikeListInputBoundary;
 
 public class LikeListController {
-
+/** This controller class accepts user input and connects it to back end likeList functions **/
     final LikeListInputBoundary userInput;
 
     public LikeListController(LikeListInputBoundary likeListUserInput){
         this.userInput = likeListUserInput;
     }
     public void addLike(String userId, String otherUserId){
-        userInput.addLike(userId, otherUserId);
+        this.userInput.addLike(userId, otherUserId);
     }
     public void removeLike(String userId, String otherUserId){
-        userInput.removeLike(userId, otherUserId);
+        this.userInput.removeLike(userId, otherUserId);
     }
 }
--- app/src/main/java/com/courseproject/tindar/usecases/likelist/LikeListDsGateway.java
+++ app/src/main/java/com/courseproject/tindar/usecases/likelist/LikeListDsGateway.java
@@ -1,7 +1,8 @@
 package com.courseproject.tindar.usecases.likelist;
 
 public interface LikeListDsGateway {
-
+/** This class accesses and distributes data from database to be manipulated by likeList functions
+ * and allows for users to like and match with each other **/
     boolean checkLiked(String userId, String otherUserId);
 
     void addToMatched(String userId, String otherUserId);
