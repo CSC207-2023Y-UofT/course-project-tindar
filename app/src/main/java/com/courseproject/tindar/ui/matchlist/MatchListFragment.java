3d6fd7bb6c7968e1bb37a1dd99d5db0e9680ae08
Jennifer Yang <76457965+jenniferjinyoungyang@users.noreply.github.com>
2023-08-01 19:15:53 -0400

    [Feature 36] connects Profile screen and Filters screen to the Navigation Drawer (#45)

--- app/src/main/java/com/courseproject/tindar/ui/matchlist/MatchListFragment.java
+++ app/src/main/java/com/courseproject/tindar/ui/matchlist/MatchListFragment.java
@@ -10,18 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.courseproject.tindar.databinding.FragmentMatchlistBinding;
+import com.courseproject.tindar.databinding.FragmentMatchListBinding;
 
 public class MatchListFragment extends Fragment {
 
-    private FragmentMatchlistBinding binding;
+    private FragmentMatchListBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MatchListViewModel galleryViewModel =
                 new ViewModelProvider(this).get(MatchListViewModel.class);
 
-        binding = FragmentMatchlistBinding.inflate(inflater, container, false);
+        binding = FragmentMatchListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
