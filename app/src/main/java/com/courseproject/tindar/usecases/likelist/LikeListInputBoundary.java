1f444aa9e3cc088f9e4e62a0f36372e36aa980b8
kresimirgotovac <133273109+kresimirgotovac@users.noreply.github.com>
2023-08-01 22:21:18 -0400

    [Feature 7] adds controller and interactor to like and unlike (#26)

---
+++ app/src/main/java/com/courseproject/tindar/usecases/likelist/LikeListInputBoundary.java
@@ -0,0 +1,14 @@
+package com.courseproject.tindar.usecases.likelist;
+
+public interface LikeListInputBoundary {
+    /**
+     * This interface initializes a like interaction between two users. the current user userID
+     * will be added to other likeList. In the event that userId is already in other likeList
+     * they will be matched. removeLike Removes userId from otherUserId like list, and removes
+     * the users from each others matchLists.
+     **/
+    void addLike(String userId, String otherUserId);
+
+    void removeLike(String userId, String otherUserId);
+}
+
