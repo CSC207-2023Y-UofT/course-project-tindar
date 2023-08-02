30fe8755d9d45f4e0693627cdf5fe869c9eaad00
chocolatecakes <serena.he.03@gmail.com>
2023-08-02 13:12:19 -0400

    view model database debugging

--- app/src/main/java/com/courseproject/tindar/ui/home/HomeViewModel.java
+++ app/src/main/java/com/courseproject/tindar/ui/home/HomeViewModel.java
@@ -1,21 +1,33 @@
 package com.courseproject.tindar.ui.home;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.courseproject.tindar.ds.DatabaseHelper;
+import com.courseproject.tindar.usecases.editfilters.EditFiltersDsResponseModel;
 import com.courseproject.tindar.usecases.editprofile.EditProfileDsResponseModel;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class HomeViewModel extends ViewModel {
 
-    private ArrayList<String> potentialProfiles
+    private ArrayList<String> potentialProfiles;
+    private String DisplayNameText;
+    private String GenderText;
+    private Date BirthdayText;
+    private String LocationText;
+    private String AboutMeText;
+    private String ProfilePicture;
 
     public HomeViewModel() {
+
+
         ArrayList<String> potentialProfiles = new ArrayList<String>();
 
-        DatabaseHelper databaseHelper = new DatabaseHelper();
-        readProfile(potentialProfiles.get(0));
+        EditProfileDsResponseModel readProfileResponseModel =
+                new EditProfileDsResponseModel(DisplayNameText, BirthdayText, GenderText, LocationText, ProfilePicture, AboutMeText);
+        readProfileResponseModel.readProfile(potentialProfiles.get(0));
 
     }
 
---
+++ app/src/test/java/com/courseproject/tindar/presenters/ViewProfilesPresenterUnitTest.java
@@ -0,0 +1,14 @@
+package com.courseproject.tindar.presenters;
+
+import static org.junit.Assert.assertEquals;
+
+import com.courseproject.tindar.usecases.editfilters.EditFiltersDsResponseModel;
+import com.courseproject.tindar.usecases.editfilters.EditFiltersInputBoundary;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ViewProfilesPresenterUnitTest {
+}
