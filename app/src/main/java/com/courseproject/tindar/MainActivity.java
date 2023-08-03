10d1f0f34ca765149061b7f4e2186680aabad2d7
chocolatecakes <serena.he.03@gmail.com>
2023-08-03 12:15:18 -0400

    login button checks for correct password

--- app/src/main/java/com/courseproject/tindar/MainActivity.java
+++ app/src/main/java/com/courseproject/tindar/MainActivity.java
@@ -2,10 +2,17 @@ package com.courseproject.tindar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.courseproject.tindar.controllers.login.LoginController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,16 +28,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = findViewById(R.id.button_login);
-        Button signupButton = findViewById(R.id.button_login);
+        Button signupButton = findViewById(R.id.button_signup);
 
+        LoginController loginController = new LoginController();
         passwordText = (EditText)findViewById(R.id.password);
         emailText = (EditText)findViewById(R.id.email);
 
         loginButton.setOnClickListener(view -> {
+            email = emailText.getText().toString();
+            password = passwordText.getText().toString();
 
-            Intent intent = new Intent(MainActivity.this, BlankNavActivity.class);
-            intent.putExtra("user_id", "1");
-            startActivity(intent);
+            if (password.equals(loginController.getPassword(email))) {
+                Intent intent = new Intent(MainActivity.this, BlankNavActivity.class);
+                intent.putExtra("user_id", "1");
+                startActivity(intent);
+            } else {
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_wrong_credentials, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+            }
         });
 
         signupButton.setOnClickListener(view -> {
--- app/src/main/java/com/courseproject/tindar/controllers/login/LoginController.java
+++ app/src/main/java/com/courseproject/tindar/controllers/login/LoginController.java
@@ -4,10 +4,8 @@ import com.courseproject.tindar.usecases.editprofile.EditProfileDsResponseModel;
 
 public class LoginController {
     public LoginController(){
-
     }
 
-    // get profile by email, return correct password
     public String getPassword(String email){
         return "password";
     }
