efcfaade3a6f4f8f0548f5f9b28996c3ea7bfc5c
navarhontes <sophiawan01@gmail.com>
2023-08-03 15:44:24 -0400

    Added some TODOs and tests to help ensure that scrolling works

--- app/src/main/java/com/courseproject/tindar/ChatActivity.java
+++ app/src/main/java/com/courseproject/tindar/ChatActivity.java
@@ -22,6 +22,12 @@ public class ChatActivity extends AppCompatActivity {
      * Display details are in activity_chat.xml.
      */
 
+    /*
+        TODO:
+        - un-hardcode "Username" to be the actual conversation partner's display name
+        - un-hardcode userID to be the current user's userID
+     */
+
     // Variable here for getting other userID and display name
     // variable here for storing your userID
     EditText chatInput;
@@ -52,14 +58,24 @@ public class ChatActivity extends AppCompatActivity {
         this.chatRecyclerView.setLayoutManager(linearLayoutManager);
     }
 
-    //TODO: this should actually call a different class for the messages. probably conversation.
+    //TODO: this should be implemented properly
+    // and call a different class for the messages. probably conversation.
     private void loadMessages(){
         TindarMessage testMessage1 = new TindarMessage("first message sent", new Timestamp(2023, 02, 25, 18, 0, 0, 0),"user1", "user2");
         TindarMessage testMessage2 = new TindarMessage("second message sent", new Timestamp(2023, 03, 25, 18, 0, 0, 0),"user1", "user2");
         TindarMessage testMessage3 = new TindarMessage("first message received", new Timestamp(2023, 04, 25, 18, 0, 0, 0),"user2", "user1");
+        TindarMessage testMessage4 = new TindarMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam maximus erat nisl, a scelerisque leo euismod nec. Ut dapibus auctor augue, quis tempor tellus tincidunt vel. Fusce ut odio mauris. Nam nec finibus enim. Duis et nisi tristique, luctus leo id, facilisis dolor. Phasellus ac auctor odio, non mollis magna. Suspendisse tortor ipsum, consectetur vitae metus et, accumsan luctus erat. Sed gravida, ipsum vel mattis maximus, orci arcu convallis nunc, sed sagittis metus felis ac sapien. Integer non pellentesque massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et dignissim erat. Integer at nibh ac mi ultricies pulvinar. Morbi nec arcu nisi. Duis eu ligula auctor, dictum tortor a, condimentum velit. Suspendisse potenti. Sed fermentum lobortis blandit.", new Timestamp(2023, 05, 25, 18, 0, 0, 0),"user1", "user2");
+        TindarMessage testMessage5 = new TindarMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam maximus erat nisl, a scelerisque leo euismod nec. Ut dapibus auctor augue, quis tempor tellus tincidunt vel. Fusce ut odio mauris. Nam nec finibus enim. Duis et nisi tristique, luctus leo id, facilisis dolor. Phasellus ac auctor odio, non mollis magna. Suspendisse tortor ipsum, consectetur vitae metus et, accumsan luctus erat. Sed gravida, ipsum vel mattis maximus, orci arcu convallis nunc, sed sagittis metus felis ac sapien. Integer non pellentesque massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et dignissim erat. Integer at nibh ac mi ultricies pulvinar. Morbi nec arcu nisi. Duis eu ligula auctor, dictum tortor a, condimentum velit. Suspendisse potenti. Sed fermentum lobortis blandit.", new Timestamp(2023, 05, 25, 18, 0, 0, 0),"user2", "user1");
+
         this.loadedMessages = new ArrayList<MessageModel>();
         this.loadedMessages.add(testMessage1);
         this.loadedMessages.add(testMessage2);
-        this.loadedMessages.add(testMessage2);
+        this.loadedMessages.add(testMessage3);
+        this.loadedMessages.add(testMessage3);
+        this.loadedMessages.add(testMessage3);
+        this.loadedMessages.add(testMessage3);
+        this.loadedMessages.add(testMessage3);
+        this.loadedMessages.add(testMessage4);
+        this.loadedMessages.add(testMessage5);
     }
 }
