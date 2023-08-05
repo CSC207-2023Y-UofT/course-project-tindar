4c2e76897232c655f0b602f3b5bc855e62e1692e
yiwxng <lindawang0530@gmail.com>
2023-08-05 16:08:19 -0400

    minor changes for testing

--- app/src/main/java/com/courseproject/tindar/ui/chats/ConvoFragment.java
+++ app/src/main/java/com/courseproject/tindar/ui/chats/ConvoFragment.java
@@ -3,13 +3,20 @@ package com.courseproject.tindar.ui.chats;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.courseproject.tindar.R;
 import com.courseproject.tindar.databinding.FragmentChatsBinding;
+import com.courseproject.tindar.entities.ConvoList;
+import com.courseproject.tindar.entities.ConvoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is a frontend of Conversation list, in the format of a fragment from the side menu bar
@@ -55,22 +62,52 @@ public class ConvoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding  = FragmentChatsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
 
-//        // Set the adapter
-//        if (view instanceof RecyclerView) {
-//            Context context = view.getContext();
-//            RecyclerView recyclerView = (RecyclerView) view;
-//            if (mColumnCount <= 1) {
-//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-//            } else {
-//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-//            }
-//            recyclerView.setAdapter(new MychatsRecyclerViewAdapter(PlaceholderContent.ITEMS));
-//        }
-        return root;
+        // Find the RecyclerView in the layout
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_view_convo_list);
+
+        // Create a list of ConvoModel instances (for testing purposes)
+        List<ConvoModel> convoList = new ArrayList<>();
+        convoList.add(new ConvoList("User1", "Hello!", "2023-08-05 15:30"));
+        convoList.add(new ConvoList("User2", "Hi there!", "2023-08-05 16:45"));
+
+        // Create the adapter and set it to the RecyclerView
+        ConvoListRecyclerViewAdapter adapter = new ConvoListRecyclerViewAdapter(convoList);
+        recyclerView.setAdapter(adapter);
+
+        // Set the RecyclerView layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
     }
+
+    // ... other code ...
 }
+
+
+
+
+
+
+//@Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        binding  = FragmentChatsBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+////        // Set the adapter
+////        if (view instanceof RecyclerView) {
+////            Context context = view.getContext();
+////            RecyclerView recyclerView = (RecyclerView) view;
+////            if (mColumnCount <= 1) {
+////                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+////            } else {
+////                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+////            }
+////            recyclerView.setAdapter(new MychatsRecyclerViewAdapter(PlaceholderContent.ITEMS));
+////        }
+//        return root;
+//    }
+//}
