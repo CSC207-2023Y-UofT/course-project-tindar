63ab4d93226c935a5520ff83cedc8f163604bbdc
yiwxng <lindawang0530@gmail.com>
2023-08-05 19:30:55 -0400

    trying again convofrontend

--- app/src/main/java/com/courseproject/tindar/ui/chats/ConvoListRecyclerViewAdapter.java
+++ app/src/main/java/com/courseproject/tindar/ui/chats/ConvoListRecyclerViewAdapter.java
@@ -1,18 +1,18 @@
 package com.courseproject.tindar.ui.chats;
 
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.courseproject.tindar.databinding.FragmentChatsBinding;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.courseproject.tindar.R;
 import com.courseproject.tindar.entities.ConvoModel;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a conversation preview.
- */
 public class ConvoListRecyclerViewAdapter extends RecyclerView.Adapter<ConvoListRecyclerViewAdapter.ViewHolder> {
 
     private final List<ConvoModel> mValues;
@@ -21,21 +21,22 @@ public class ConvoListRecyclerViewAdapter extends RecyclerView.Adapter<ConvoList
         mValues = items;
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        FragmentChatsBinding binding = FragmentChatsBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new ViewHolder(binding);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_chat_list, parent, false);
+        return new ViewHolder(view);
     }
 
-
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ConvoModel convo = mValues.get(position);
 
         // Set the values for the DisplayName, last text preview, and time in the TextViews
-        holder.mUsernameView.setText(mValues.get(position).getDisplayName());
-        holder.mLastTextView.setText(mValues.get(position).getLastMessageDisplayText());
-        holder.mTimeView.setText(mValues.get(position).getLastMessageTime());
+        holder.mUsernameView.setText(convo.getDisplayName());
+        holder.mLastTextView.setText(convo.getLastMessageDisplayText());
+        holder.mTimeView.setText(convo.getLastMessageTime());
     }
 
     @Override
@@ -43,27 +44,87 @@ public class ConvoListRecyclerViewAdapter extends RecyclerView.Adapter<ConvoList
         return mValues.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public final TextView mUsernameView;
         public final TextView mLastTextView;
         public final TextView mTimeView;
-        public ConvoModel mItem;
 
-        public ViewHolder(FragmentChatsBinding binding) {
-            super(binding.getRoot());
-            mUsernameView = binding.userNameText;
-            mLastTextView = binding.lastMessageText;
-            mTimeView = binding.lastMessageTimeText;
+        public ViewHolder(View view) {
+            super(view);
+            mUsernameView = view.findViewById(R.id.user_name_text);
+            mLastTextView = view.findViewById(R.id.last_message_text);
+            mTimeView = view.findViewById(R.id.last_message_time_text);
         }
-    @Override
-    public String toString() {
-            return super.toString() + " '" + mLastTextView.getText() + "'";
-        }
-
-        // Rest of the ViewHolder code...
     }
-
-
-
-
 }
+
+
+//package com.courseproject.tindar.ui.chats;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//import android.view.LayoutInflater;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import com.courseproject.tindar.databinding.FragmentChatsBinding;
+//import com.courseproject.tindar.entities.ConvoModel;
+//
+//import java.util.List;
+//
+///**
+// * {@link RecyclerView.Adapter} that can display a conversation preview.
+// */
+//public class ConvoListRecyclerViewAdapter extends RecyclerView.Adapter<ConvoListRecyclerViewAdapter.ViewHolder> {
+//
+//    private final List<ConvoModel> mValues;
+//
+//    public ConvoListRecyclerViewAdapter(List<ConvoModel> items) {
+//        mValues = items;
+//    }
+//
+//    @Override
+//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        FragmentChatsBinding binding = FragmentChatsBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+//        return new ViewHolder(binding);
+//    }
+//
+//
+//    @Override
+//    public void onBindViewHolder(final ViewHolder holder, int position) {
+//        holder.mItem = mValues.get(position);
+//
+//        // Set the values for the DisplayName, last text preview, and time in the TextViews
+//        holder.mUsernameView.setText(mValues.get(position).getDisplayName());
+//        holder.mLastTextView.setText(mValues.get(position).getLastMessageDisplayText());
+//        holder.mTimeView.setText(mValues.get(position).getLastMessageTime());
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return mValues.size();
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public final TextView mUsernameView;
+//        public final TextView mLastTextView;
+//        public final TextView mTimeView;
+//        public ConvoModel mItem;
+//
+//        public ViewHolder(FragmentChatsBinding binding) {
+//            super(binding.getRoot());
+//            mUsernameView = binding.userNameText;
+//            mLastTextView = binding.lastMessageText;
+//            mTimeView = binding.lastMessageTimeText;
+//        }
+//    @Override
+//    public String toString() {
+//            return super.toString() + " '" + mLastTextView.getText() + "'";
+//        }
+//
+//        // Rest of the ViewHolder code...
+//    }
+//
+//
+//
+//
+//}
