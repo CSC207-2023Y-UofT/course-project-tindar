8f56ec8d10cb055ccce4937ffe5c1a8cd1ed9202
kresimirgotovac <133273109+kresimirgotovac@users.noreply.github.com>
2023-08-12 13:04:52 -0400

    [Task 14] adds Java doc for like list / match list (#103)

--- app/src/main/java/com/courseproject/tindar/usecases/likelist/LikeListResponseModel.java
+++ app/src/main/java/com/courseproject/tindar/usecases/likelist/LikeListResponseModel.java
@@ -1,18 +1,39 @@
 package com.courseproject.tindar.usecases.likelist;
 
+/** This class allows the match list fragment to simultaneously display users displayNames and
+ * send the userIds to ChatActivity so when a  user is clicked in the match list, we know which
+ * conversation to transition to.
+ */
 public class LikeListResponseModel {
+
+    /** List of userIds */
     private final String[] userIds;
+
+    /** List of displayNames */
     private final String[] displayNames;
 
+    /**
+     * Construct a LikeListResponseModel object
+     *
+     * @param userIds list of userIds in match list
+     * @param displayNames list of displayNames in match list
+     */
     public LikeListResponseModel(String[] userIds, String[] displayNames) {
         this.userIds = userIds;
         this.displayNames = displayNames;
     }
     // Two simple getter functions
+
+    /** gets userIds
+     * @return return userIds
+     */
     public String[] getUserIds() {
         return userIds;
     }
 
+    /** gets displayNames
+     *@return  return displayNames
+     */
     public String[] getDisplayNames() {
         return displayNames;
     }
