bf221263351a420778ed0404ee70325957871287
navarhontes <sophiawan01@gmail.com>
2023-08-12 16:08:01 -0400

    Updated ConversationModel after discussion with Yi

--- app/src/main/java/com/courseproject/tindar/entities/ConversationModel.java
+++ app/src/main/java/com/courseproject/tindar/entities/ConversationModel.java
@@ -1,4 +1,19 @@
 package com.courseproject.tindar.entities;
 
+/**
+ * Used for manipulating conversation records with ChatDatabaseGateways
+ */
 public interface ConversationModel{
+
+    /**
+     * Returns the ID of representation of a conversation
+     * @return this conversation's ID
+     */
+    int getConversationId();
+
+    /**
+     * Returns the users in this conversation.
+     * @return the IDs of the users involved in this conversation, in ascending numerical order
+     */
+    int[] getUsers();
 }
