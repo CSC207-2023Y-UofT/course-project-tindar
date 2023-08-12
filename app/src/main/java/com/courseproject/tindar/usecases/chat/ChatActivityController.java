b57a33706246121a601aef8cfbf7493dd824e53a
navarhontes <sophiawan01@gmail.com>
2023-08-12 17:04:22 -0400

    Deleted chat presenter and controller since they still exist in another branch

--- app/src/main/java/com/courseproject/tindar/usecases/chat/ChatActivityController.java
+++
@@ -1,8 +0,0 @@
-package com.courseproject.tindar.usecases.chat;
-
-public class ChatActivityController {
-    // should have a method called when something is sent
-    public void sendMessage(){
-
-    }
-}
