1bb5afb79ba98a7920006a10a56c9092a1eb1b40
navarhontes <sophiawan01@gmail.com>
2023-08-13 12:27:26 -0400

    Didn't undo registered renaming properly

--- app/src/main/java/com/courseproject/tindar/ui/conversationlist/ConversationListFragment.java
+++ app/src/main/java/com/courseproject/tindar/ui/conversationlist/ConversationListFragment.java
@@ -1,17 +1,18 @@
 package com.courseproject.tindar.ui.conversationlist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.courseproject.tindar.R;
+import com.courseproject.tindar.ui.chat.ChatActivity;
 import com.courseproject.tindar.usecases.conversationlist.ConversationResponseModel;
 
 import java.util.ArrayList;
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 /**
  * A fragment representing a list of Items.
  */
-public class ConversationFragment extends Fragment {
+public class ConversationListFragment extends Fragment {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
@@ -30,13 +31,13 @@ public class ConversationFragment extends Fragment {
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public ConversationFragment() {
+    public ConversationListFragment() {
     }
 
     // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
-    public static ConversationFragment newInstance(int columnCount) {
-        ConversationFragment fragment = new ConversationFragment();
+    public static ConversationListFragment newInstance(int columnCount) {
+        ConversationListFragment fragment = new ConversationListFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
@@ -60,16 +61,39 @@ public class ConversationFragment extends Fragment {
         convos.add(new ConversationResponseModel("lisbeth", "how do you do", "20:20"));
         convos.add(new ConversationResponseModel("mikael", "silence", "20:50"));
         // Set the adapter
-        if (view instanceof RecyclerView) {
+
+        RecyclerView recyclerView = view.findViewById(R.id.list);
+        if (recyclerView != null) {
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyConversationRecyclerViewAdapter(convos));
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+            MyConversationRecyclerViewAdapter adapter = new MyConversationRecyclerViewAdapter(convos);
+            recyclerView.setAdapter(adapter);
+
+            // Set click listener for conversation items
+            adapter.setOnItemClickListener(conversation -> {
+                // Handle the click here, navigate to ChatActivity
+                Intent intent = new Intent(requireActivity(), ChatActivity.class);
+                intent.putExtra("conversation_partner_name", conversation.getUserName());
+                // Pass any other necessary data
+                startActivity(intent);
+            });
         }
+
         return view;
-    }
-}
+    }   }
+//        RecyclerView recyclerView = null;
+//        if (view instanceof RecyclerView) {
+//            Context context = view.getContext();
+//            recyclerView = (RecyclerView) view;
+//            if (mColumnCount <= 1) {
+//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+//            } else {
+//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+//            }
+//            recyclerView.setAdapter(new MyConversationRecyclerViewAdapter(convos));
+//        }
+//
+//
+//        return view;
+//    }
